e9f50d8d02ef4ff3fff6deb0909c0464b27f9330
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-07 12:44:54 +0300

    Реализация окончаний на Intl.PluralRules

--- my-app/src/components/footer/TodoCount.jsx
+++ my-app/src/components/footer/TodoCount.jsx
@@ -1,41 +1,23 @@
 import styles from "../footer/Footer.module.scss";
 
 const pluralize = (num, [one, some, many]) => {
-  let number = num;
-
-  if (number === null) {
-    return "";
+  const pluralRules = new Intl.PluralRules("ru-RU"); 
+ const pluralForm = pluralRules.select(num); 
+ 
+  switch (pluralForm) {
+    case "one":
+      return one;
+    case "few":
+      return some;
+    default:
+      return many;
   }
-
-  number = Number(number);
-
-  if (Number.isNaN(number)) {
-    return "";
-  }
-
-  number %= 100;
-
-  if (number >= 5 && number <= 20) {
-    return many;
-  }
-
-  number %= 10;
-
-  if (number === 1) {
-    return one;
-  }
-
-  if (number >= 2 && number <= 4) {
-    return some;
-  }
-
-  return many;
 };
 
 const TodoCount = ({ length }) => {
   return (
     <p className={styles.footer__text}>
-      {length} {pluralize(length, ["задача", "задачи", "задач"])}
+       {length} {pluralize(length, ["задача", "задачи", "задач"])}
     </p>
   );
 };
