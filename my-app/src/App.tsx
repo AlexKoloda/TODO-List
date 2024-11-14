3fa33eaebd34c440bb4b7b7627042fe33d0802b0
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-14 20:26:01 +0300

    Change structure

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,15 +2,15 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import FooterDescription from "./components/FooterDescription/FooterDescription";
 import Footer from "./components/Footer/Footer";
-import Form from "./components/Form/Form";
 import List from "./components/List/List";
 import ToggleButton from "./components/ToggleButton/ToggleButton";
+import InputTodo from "./components/InputTodo/InputTodo";
 
 function App() {
   return (
     <div className="main__container">
       <Header />
-      <Form />
+      <InputTodo />
       <ToggleButton />
       <List />
       <Footer />
