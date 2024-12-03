8023e20bf3bee9d3eb47b817932a89996145312e
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-03 17:54:53 +0300

    Add routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,26 @@
-import "./App.css";
-import Header from "./components/Header/Header";
-import FooterDescription from "./components/FooterDescription/FooterDescription";
-import Footer from "./components/Footer/Footer";
-import List from "./components/List/List";
-import ToggleButton from "./components/ToggleButton/ToggleButton";
-import TodoForm from "./components/TodoForm/TodoForm";
-
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { TodoPage } from './pages/TodoPage/Todo';
+import { NotFoundPage } from './pages/NotFound/NotFound';
+import { SignIn } from './pages/SignIn/SignIn';
+import { SignUp } from './pages/SignUp/SignUp';
+import { Layout } from './components/Layout/Layout';
+import { AuthRequire } from './hoc/AuthRequire';
 
 function App() {
   return (
-    <div className="main__container">
-      <Header />
-      <TodoForm/>
-      <ToggleButton />
-      <List />
-      <Footer />
-      <FooterDescription />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+       <Route path='sign-in' element={<SignIn />} />
+       <Route path='sign-up' element={<SignUp />} />
+       <Route index element={
+         <AuthRequire>
+        <TodoPage />
+        </AuthRequire>
+        }/>
+       <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
 
