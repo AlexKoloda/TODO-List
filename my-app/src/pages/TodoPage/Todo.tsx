1e88acba956b16b19cd4fae9b0e284c4a177f44e
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-03 17:56:07 +0300

    Add todo page

---
+++ my-app/src/pages/TodoPage/Todo.tsx
@@ -0,0 +1,15 @@
+import Footer from '../../components/Footer/Footer';
+import List from '../../components/List/List';
+import TodoForm from '../../components/TodoForm/TodoForm';
+import ToggleButton from '../../components/ToggleButton/ToggleButton';
+
+export const TodoPage = () => {
+  return (
+    <div className='main__container'>
+      <TodoForm />
+      <ToggleButton />
+      <List />
+      <Footer />
+    </div>
+  );
+}
