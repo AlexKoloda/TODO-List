b90ff03032f3636acf49026776735629e5c6097d
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-04 18:53:38 +0300

    Change input structure

---
+++ my-app/src/components/Input/Input.tsx
@@ -0,0 +1,27 @@
+export interface InputProps {
+  name?: string;
+  type: string;
+  onBlur?: () => void;
+  initialValue?: string;
+  inputClassName?: string;
+  placeholderText?: string;
+  inputValue?: string;
+  onValueChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+  return (
+    <input
+      name={props.name}
+      type={props.type}
+      placeholder={props.placeholderText}
+      value={props.inputValue}
+      onChange={props.onValueChange}
+      className={props.inputClassName}
+      autoFocus
+      onBlur={props.onBlur}
+    />
+  );
+};
+
+export default Input;
