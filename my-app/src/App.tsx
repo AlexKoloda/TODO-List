7882a87cc8c3de45c4df93932c1c16c17656f4d4
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-04 18:52:11 +0300

    Delete AuthRequire

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,23 +5,20 @@ import { NotFoundPage } from './pages/NotFound/NotFound';
 import { SignIn } from './pages/SignIn/SignIn';
 import { SignUp } from './pages/SignUp/SignUp';
 import { Layout } from './components/Layout/Layout';
-import { AuthRequire } from './hoc/AuthRequire';
+//import { AuthRequire } from './hoc/AuthRequire';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+       <Route path='*' element={<NotFoundPage />} />
+       <Route path="/" element={<Layout />}>
+       <Route index element={<TodoPage />}/>
        <Route path='sign-in' element={<SignIn />} />
        <Route path='sign-up' element={<SignUp />} />
-       <Route index element={
-         <AuthRequire>
-        <TodoPage />
-        </AuthRequire>
-        }/>
-       <Route path='*' element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
+  
 }
 
 export default App;
