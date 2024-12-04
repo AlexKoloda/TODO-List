35c840bae7eb0ad36431c5d0a4feedb4d5742b02
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-04 18:54:00 +0300

    Refactor list with new component

--- my-app/src/components/ListItem/ListItem.tsx
+++ my-app/src/components/ListItem/ListItem.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { toggleComplete,deleteTodo,Todo,editTodo,} from "../../store/todoSlice";
-import Form from "../Form/Form";
-import styles from "./ListItem.module.scss";
-import cn from "classnames";
-import { useAppDispatch } from "../../hook";
+import { useState } from 'react';
+import { toggleComplete,deleteTodo,Todo,editTodo } from '../../store/todoSlice';
+import Form from '../Form/Form';
+import styles from './ListItem.module.scss';
+import cn from 'classnames';
+import { useAppDispatch } from '../../hook';
 
 interface ListItemProps {
   todo: Todo;
@@ -12,6 +12,13 @@ interface ListItemProps {
 const ListItem: React.FC<ListItemProps> = ({ todo }) => {
   const dispatch = useAppDispatch();
   const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(todo.text);
+
+  const handleValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('click')
+    setText(event.currentTarget.value);
+  };
+
 
   const handleClick = () => {
     dispatch(toggleComplete(todo.id));
@@ -25,14 +32,25 @@ const ListItem: React.FC<ListItemProps> = ({ todo }) => {
     dispatch(deleteTodo(todo.id));
   };
 
-  const handleSubmitChanges = (text: string) => {
-    dispatch(editTodo({ id: todo.id, text }));
+  const handleSubmitChanges = () => {
+    dispatch(editTodo({ id: todo.id, text}));
     setIsEditing(false);
   };
 
   const handleClickOutside = () => {
     setIsEditing(false);
   };
+
+  const inputs = [
+    {
+      type: 'text',
+      inputClassName: styles.list__input,
+      placeholderText: 'Что нужно сделать?',
+      inputValue: text,   
+      onBlur: handleClickOutside, 
+      onValueChange: handleValueChange,
+    },
+  ];
 
   return (
     <li className={styles.list__item}>
@@ -50,21 +68,18 @@ const ListItem: React.FC<ListItemProps> = ({ todo }) => {
         })}
         onDoubleClick={handleDoubleClick}
       >
-        {isEditing ? (
+         {isEditing ? (
           <Form
+            inputs={inputs}
             onSubmit={handleSubmitChanges}
-            initialValue={todo.text}
-            inputClassName={styles.list__input}
-            buttonClassName={styles.list__button}
-            onBlur={handleClickOutside}
           />
         ) : (
           todo.text
-        )}
+        )} 
       </div>
       <button
         className={styles.list__delete}
-        type="button"
+        type='button'
         onClick={handleClickDelete}
       >
         ✕
