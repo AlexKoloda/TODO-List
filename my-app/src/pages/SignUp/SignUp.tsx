81d7961571f8dbc6bea2f0aca949ecbb0553b595
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-04 18:50:18 +0300

    Fix sign-up

--- my-app/src/pages/SignUp/SignUp.tsx
+++ my-app/src/pages/SignUp/SignUp.tsx
@@ -1,44 +1,87 @@
 import { Link } from 'react-router-dom';
 import Form from '../../components/Form/Form';
 import styles from './SignUp.module.scss';
+import { useState } from 'react';
 
 export const SignUp = () => {
+  const [createUser, setCreateUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    dateBirth: '',
+  });
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCreateUser((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const inputs = [
+    {
+      name: 'firstName',
+      type: 'text',
+      inputClassName: styles.sign_up__input,
+      placeholderText: 'Введите ваше имя',
+      inputValue: createUser.firstName,
+      onValueChange: handleChangeValue,
+    },
+    {
+      name: 'lastName',
+      type: 'text',
+      inputClassName: styles.sign_up__input,
+      placeholderText: 'Введите вашу фамилию',
+      inputValue: createUser.lastName,
+      onValueChange: handleChangeValue,
+    },
+    {
+      name: 'email',
+      type: 'email',
+      inputClassName: styles.sign_up__input,
+      placeholderText: 'Введите ваш email',
+      inputValue: createUser.email,
+      onValueChange: handleChangeValue,
+    },
+    {
+      name: 'password',
+      type: 'password',
+      inputClassName: styles.sign_up__input,
+      placeholderText: 'Введите ваш пароль',
+      inputValue: createUser.password,
+      onValueChange: handleChangeValue,
+    },
+    {
+      name: 'dateBirth',
+      type: 'text',
+      inputClassName: styles.sign_up__input,
+      placeholderText: 'Введите вашу дату рождения',
+      inputValue: createUser.dateBirth,
+      onValueChange: handleChangeValue,
+    },
+  ];
+
   return (
     <div className='main__container'>
-      <h1 className={styles.signUp__title}>Добро пожаловать</h1>
-      <p className={styles.signUp__description}>
+      <h1 className={styles.sign_up__title}>Добро пожаловать</h1>
+      <p className={styles.sign_up__description}>
         Создайте аккаунт, что бы пользоваться индивидуальным списком задач.
       </p>
-      <form>
 
       <Form
-        placeholderText={'Введите имя'}
-        inputClassName={styles.signUp__input}
-        />
-      <Form
-        placeholderText={'Введите Фамилию?'}
-        inputClassName={styles.signUp__input}
-        />
-      <Form
-        placeholderText={'Введите email?'}
-        inputClassName={styles.signUp__input}
-        />
-      <Form
-        placeholderText={'Введите пароль?'}
-        inputClassName={styles.signUp__input}
-        
-        />
-      <Form
-        placeholderText={'Введите дату рождения?'}
-        inputClassName={styles.signUp__input}
-        buttonClassName={styles.signUp__submit}
-        buttonTitle={'Создать'}      />
-      </form>
-      <p className={styles.signUp__description}>
-        Все поля необходимо заполнить.
-        Уже есть аккаунт? <Link className={styles.signUp__link} to="/sign-in">Войдите</Link>
+        formClassName = {styles.sign_up__form}
+        buttonClassName={styles.sign_up__submit}
+        buttonTitle={'Создать'}
+        inputs={inputs}
+      />
+
+      <p className={styles.sign_up__description}>
+        Все поля необходимо заполнить. Уже есть аккаунт?{' '}
+        <Link className={styles.sign_up__link} to='/sign-in'>
+          Войдите
+        </Link>
       </p>
-
     </div>
   );
 };
