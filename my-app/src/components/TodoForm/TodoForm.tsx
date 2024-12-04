02250493d5a623f5f039cf5837deb7e1e7604ff2
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-04 18:54:32 +0300

    Refactor Todo form with new components

--- my-app/src/components/TodoForm/TodoForm.tsx
+++ my-app/src/components/TodoForm/TodoForm.tsx
@@ -1,23 +1,39 @@
-import styles from "./TodoForm.module.scss";
-import { useAppDispatch } from "../../hook";
-import { addTodos } from "../../store/todoSlice";
-import Form from "../Form/Form";
+import styles from './TodoForm.module.scss';
+import { useAppDispatch } from '../../hook';
+import { addTodos } from '../../store/todoSlice';
+import Form from '../Form/Form';
+import { useState } from 'react';
 
 const TodoForm: React.FC = () => {
+  const [todoText, setTodoText] = useState('');
+  
+  const handleValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoText(event.target.value);
+  };
+
   const dispacth = useAppDispatch();
 
-  const addTodo = (inputValue: string) => {
-    dispacth(addTodos(inputValue));
+  const addTodo = () => {
+    dispacth(addTodos(todoText));
+    setTodoText('')
   };
 
   return (
     <div className={styles.form__todo}>
       <Form
+      formClassName={styles.form}
+        inputs={[
+          {
+            type: 'text',
+            inputClassName: styles.form__input,
+            placeholderText: 'Что нужно сделать?',
+            onValueChange: handleValueChange,
+            inputValue: todoText,
+          },
+        ]}
         onSubmit={addTodo}
-        inputClassName={styles.form__input}
         buttonClassName={styles.form__submit}
-        buttonTitle={"ок"}
-        placeholderText= {"Что нужно сделать?"}
+        buttonTitle={'ок'}
       />
     </div>
   );
