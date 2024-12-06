0dcddec1e815daedff445dd02b28bf564ff1cb78
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-06 18:20:30 +0300

    Change types

---
+++ my-app/src/types/todo.ts
@@ -0,0 +1,16 @@
+export interface Todo {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+}
+
+export interface TodosState {
+  todos: Todo[];
+  filters?: string;
+  errors: string | null,
+}
+
+export interface UserTodo {
+  text: string;
+  isCompleted: boolean;
+}
---
+++ my-app/src/types/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  dateBirth?: string;
+}
+
+export interface UserSignIn {
+  email: string;
+  password: string;
+}
+
+export interface UserState {
+  users: User[];
+}
