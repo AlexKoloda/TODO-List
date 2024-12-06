6675f90aaabcd46051ce4e58c03c3ee3cac31181
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-06 18:22:41 +0300

    Add hook

--- my-app/src/pages/TodoPage/Todo.tsx
+++ my-app/src/pages/TodoPage/Todo.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
 import Footer from '../../components/Footer/Footer';
 import List from '../../components/List/List';
 import TodoForm from '../../components/TodoForm/TodoForm';
 import ToggleButton from '../../components/ToggleButton/ToggleButton';
-
+// TODO сделать отображение туду
 export const TodoPage = () => {
+  
+  useEffect(() => {
+    (async )
+
+  }, []);
+
   return (
     <div className='main__container'>
       <TodoForm />
