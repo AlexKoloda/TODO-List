daa21c251b6f8c5460129bb4a51a8a3e1b0a92fe
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-10 18:05:52 +0300

    Feat fetch user

--- my-app/src/store/user/userThunks.ts
+++ my-app/src/store/user/userThunks.ts
@@ -1,24 +1,30 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { User, UserSignIn } from '../../types/user';
-import { signInApi, signUpApi } from '../../api/userApi';
+import { User, UserSignIn, UserSignUp } from '../../types/user';
+import { fetchUserApi, signInApi, signUpApi } from '../../api/userApi';
 
 export const signIn = createAsyncThunk<User, UserSignIn>(
   'todos/signIn', 
-  async (params, { rejectWithValue }) => {
+  async (params) => {
     const { data } = await signInApi({
       email: params.email,
       password: params.password,
     });
     localStorage.setItem('token', data.token);
-    return data.user;
-  
+    return data.user;  
 });
 
-export const signUp = createAsyncThunk<User, User>(
+export const signUp = createAsyncThunk<User, UserSignUp>(
   'todos/signUp',
   async (user) => {
       const { data } = await signUpApi(user);
       return data.user;
   }
-
 );
+
+export const fetchUser = createAsyncThunk<User, { id: number }>( 
+  'todos/fetchUser',
+  async (params) => {
+    const { data } = await fetchUserApi(params);
+    return data;    
+  }
+) 
