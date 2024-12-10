f9882c4ca8b148a4c7944c75d7ffe72be931a4f9
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-10 18:06:21 +0300

    Feat render user name

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -10,12 +10,11 @@ text: string;
 
 const Header: React.FC<HeaderType> = (props) => { 
 const users = useAppSelector(selectUser)
-console.log(users.firstName +  " " + users.lastName)
     return (
     <>
       <div className={styles.header__box}>
         <p className={styles.header__auth}>
-         Добро пожаловать {users.firstName} !
+         Добро пожаловать {users?.firstName} !
         </p>
         <Link className={styles.header__link} to='/sign-in'>
          Сменить пользователя 
