21bc6828a8a005540a88cfe4f5bc575e983571e7
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-10 18:07:37 +0300

    Feat filters and user state

--- my-app/src/pages/TodoPage/Todo.tsx
+++ my-app/src/pages/TodoPage/Todo.tsx
@@ -3,15 +3,24 @@ import Footer from '../../components/Footer/Footer';
 import List from '../../components/List/List';
 import TodoForm from '../../components/TodoForm/TodoForm';
 import ToggleButton from '../../components/ToggleButton/ToggleButton';
-import { useAppDispatch } from '../../hook';
+import { useAppDispatch, useAppSelector } from '../../hook';
 import { fetchTodos } from '../../store/todo/todoThunks';
+import { selectFilters, selectUser } from '../../store/selectors';
+import { fetchUser } from '../../store/user/userThunks';
+import { useNavigate } from 'react-router-dom';
 
 export const TodoPage = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
-
+  const user = useAppSelector(selectUser)
+  const filters = useAppSelector(selectFilters)
+  
   useEffect(() => {
-    dispatch(fetchTodos({ filter: 'all' }));
-  }, [dispatch]);
+    if(user){
+      dispatch(fetchTodos({filter: filters}));
+      dispatch(fetchUser({id: user.id}))
+    } else navigate('/sign-in');    
+  }, [dispatch, filters, navigate, user]);
 
   return (
     <div className='main__container'>
