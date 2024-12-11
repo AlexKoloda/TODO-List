a3348031a9ccb6cc759c005dea457afdc82daf88
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-11 19:08:16 +0300

    Feat toast v.1

--- my-app/src/store/user/userThunks.ts
+++ my-app/src/store/user/userThunks.ts
@@ -1,16 +1,28 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, UserSignIn, UserSignUp } from '../../types/user';
 import { fetchUserApi, signInApi, signUpApi } from '../../api/userApi';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const signIn = createAsyncThunk<User, UserSignIn>(
   'todos/signIn', 
-  async (params) => {
-    const { data } = await signInApi({
-      email: params.email,
-      password: params.password,
-    });
-    localStorage.setItem('token', data.token);
-    return data.user;  
+  async (params, {rejectWithValue}) => {
+    try {
+      const { data } = await signInApi({
+        email: params.email,
+        password: params.password,
+      });
+      localStorage.setItem('token', data.token);
+      return data.user; 
+      
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+      toast.error(error.response?.data.message);    
+    } else if (error) {
+      console.log(error);    
+    }      
+    return rejectWithValue(error);     
+    }
 });
 
 export const signUp = createAsyncThunk<User, UserSignUp>(
@@ -21,10 +33,10 @@ export const signUp = createAsyncThunk<User, UserSignUp>(
   }
 );
 
-export const fetchUser = createAsyncThunk<User, { id: number }>( 
+export const fetchUser = createAsyncThunk<User>( 
   'todos/fetchUser',
-  async (params) => {
-    const { data } = await fetchUserApi(params);
+  async () => {
+    const { data } = await fetchUserApi();
     return data;    
   }
 ) 
