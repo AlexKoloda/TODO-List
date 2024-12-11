65fbe6511e3680743d0a895f4150605f7d62c731
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-11 19:01:09 +0300

    Feat loading state

--- my-app/src/store/user/userSlice.ts
+++ my-app/src/store/user/userSlice.ts
@@ -1,29 +1,54 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { UserState } from '../../types/user';
-import { signIn, signUp } from './userThunks';
-
-
+import { fetchUser, signIn, signUp } from './userThunks';
 
 const initialState: UserState = {
   users: null,
+  loading: true,
 };
 
 const userSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: { 
-  }, 
+  reducers: {
+
+    stopLoading(state) {
+      state.loading = false;
+    },
+
+
+  },
   extraReducers(builder) {
     builder
-    .addCase(signIn.fulfilled, (state, action) => {
-      if (action.payload) {
-        state.users = action.payload;
-      }
+      .addCase(signIn.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.users = action.payload;
+        }
+      })
+      .addCase(signUp.fulfilled, () => console.log('test'))
 
-    })
-    .addCase(signUp.fulfilled, () => console.log("test"))
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.users = action.payload;
+          state.loading = false;
+        }
+      })
+
+      .addCase(fetchUser.pending, (state) => {
+        state.loading = true;
+      })
+
+      .addCase(fetchUser.rejected, (state) => {
+        state.loading = false; 
+      })
+/*       .addCase(signIn.rejected, (state, action) => {
+        console.log(action.error.message)
+      }) */
   },
-})
+});
 
+export const {
+  stopLoading,
+} = userSlice.actions;
 
 export default userSlice.reducer;
--- my-app/src/types/user.ts
+++ my-app/src/types/user.ts
@@ -16,4 +16,5 @@ export interface UserSignIn {
 
 export interface UserState {
   users: User | null;
+  loading: boolean,
 }
