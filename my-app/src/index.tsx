0c53bbc536369fb9fbbef67c1f05c192fa0621e4
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-11 19:07:49 +0300

    Feat toast

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +15,7 @@ root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </Provider>
 );
