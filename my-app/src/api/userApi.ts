f358911c2f72cf78f95dfa71d1efedde4ff51b39
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-11 19:07:40 +0300

    Change logic get user

--- my-app/src/api/userApi.ts
+++ my-app/src/api/userApi.ts
@@ -2,13 +2,13 @@ import { User, UserSignIn, UserSignUp } from '../types/user';
 import { axiosApi } from './axios'
 
 export const signInApi = (body: UserSignIn) => {
-  return axiosApi.post<{user: User; token: string}>(`auth/sign-in`, body)
+  return axiosApi.post<{user: User; token: string}>(`/auth/sign-in`, body)
 }
 
 export const signUpApi = ( body: UserSignUp) => {
   return axiosApi.post<{user: User}>(`auth/sign-up`, body)
 }
 
-export const fetchUserApi = (params: { id: number}) => {
-  return axiosApi.get<User>('/user', {params})
+export const fetchUserApi = () => {
+  return axiosApi.get<User>('/user')
 }
