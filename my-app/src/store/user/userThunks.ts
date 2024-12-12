703051ac746ecfca22b657926b2e0174e5ff481f
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-12 18:03:25 +0300

    Feat toast for errors

--- my-app/src/store/user/userThunks.ts
+++ my-app/src/store/user/userThunks.ts
@@ -2,41 +2,50 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, UserSignIn, UserSignUp } from '../../types/user';
 import { fetchUserApi, signInApi, signUpApi } from '../../api/userApi';
 import axios from 'axios';
+import { parseError } from '../../ util/parseError';
 import { toast } from 'react-toastify';
 
 export const signIn = createAsyncThunk<User, UserSignIn>(
-  'todos/signIn', 
-  async (params, {rejectWithValue}) => {
+  'todos/signIn',
+  async (params, { rejectWithValue }) => {
     try {
       const { data } = await signInApi({
         email: params.email,
         password: params.password,
       });
       localStorage.setItem('token', data.token);
-      return data.user; 
-      
+      return data.user;
     } catch (error) {
       if (axios.isAxiosError(error)) {
-      toast.error(error.response?.data.message);    
-    } else if (error) {
-      console.log(error);    
-    }      
-    return rejectWithValue(error);     
+        toast.error(parseError(error));
+      }
+      const err = error as Error;
+      return rejectWithValue(err.name);
     }
-});
-
-export const signUp = createAsyncThunk<User, UserSignUp>(
-  'todos/signUp',
-  async (user) => {
-      const { data } = await signUpApi(user);
-      return data.user;
   }
 );
 
-export const fetchUser = createAsyncThunk<User>( 
+export const signUp = createAsyncThunk<User, UserSignUp>(
+  'todos/signUp',
+  async (user, { rejectWithValue }) => {
+    try {
+      const { data } = await signUpApi(user);
+      return data.user;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(parseError(error));
+      }
+
+      const err = error as Error;
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
+export const fetchUser = createAsyncThunk<User>(
   'todos/fetchUser',
   async () => {
-    const { data } = await fetchUserApi();
-    return data;    
-  }
-) 
+      const { data } = await fetchUserApi();
+      return data;
+    }  
+);
